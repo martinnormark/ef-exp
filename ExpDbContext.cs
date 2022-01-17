510cf4153ab070d0bb5016d53e0d3acf2765e20a
Martin H. Normark <m@martinnormark.com>
2022-01-17 22:31:53 +0100

    Initial commit

---
+++ ExpDbContext.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace EfExp
+{
+	public class ExpDbContext : DbContext
+	{
+		public DbSet<Post>? Posts { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			var range = new List<int>();
+
+			for (int i = 1; i < 15000; i++)
+			{
+				range.Add(i);
+			}
+
+			modelBuilder.Entity<Post>().HasData(range.Select(i => new Post { Id = i, Sequence = $"Seq {i}" }));
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+			=> options.UseSqlServer($"Server=.;Database=EfExp;Trusted_Connection=True;");
+	}
+
+	public class Post
+	{
+		public int Id { get; set; }
+		public string? Sequence { get; set; }
+	}
+}
