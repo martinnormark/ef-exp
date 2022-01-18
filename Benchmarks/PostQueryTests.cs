7b2545a4c5cb53dd92db3cb163fd01daad041d9f
Martin H. Normark <m@martinnormark.com>
2022-01-19 00:13:19 +0100

    Update PostQueryTests.cs

--- Benchmarks/PostQueryTests.cs
+++ Benchmarks/PostQueryTests.cs
@@ -5,7 +5,12 @@ namespace EfExp.Benchmarks
 {
 	public class PostQueryTests
 	{
-		private readonly ExpDbContext? _context = new ExpDbContext();
+		private readonly ExpDbContext _context;
+
+		public PostQueryTests()
+		{
+			_context = new ExpDbContext();
+		}
 
 		[Params(10, 100, 5000, 10000, 13500)]
 		public int IdsAbove;
@@ -13,13 +18,13 @@ namespace EfExp.Benchmarks
 		[Benchmark]
 		public void QueryWithTracking()
 		{
-			var toDelete = _context?.Set<Post>().Where(p => p.Id > IdsAbove).ToList();
+			var toDelete = _context.Set<Post>().Where(p => p.Id > IdsAbove).ToList();
 		}
 
 		[Benchmark]
 		public void QueryNoTracking()
 		{
-			var toDelete = _context?
+			var toDelete = _context
 				.Set<Post>()
 				.AsNoTracking()
 				.Where(p => p.Id > IdsAbove).ToList();
